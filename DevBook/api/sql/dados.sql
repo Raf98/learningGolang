cf8f984d9054f3c998bd13de9e154ebdafe0e1e1
raf98 <rafaelrutzsantos@gmail.com>
2023-11-16 04:11:15 -0300

    Finished lesson 106.

--- DevBook/api/sql/dados.sql
+++ DevBook/api/sql/dados.sql
@@ -7,3 +7,9 @@ VALUES
 INSERT INTO seguidores (usuario_id, seguidor_id)
 VALUES 
 (1, 2), (3, 1), (1, 3);
+
+INSERT INTO publicacoes(titulo, conteudo, autor_id)
+VALUES
+("Publicacao do Usuario 1", "Essa é a publicação do usuário 1! Oba!", 1),
+("Publicacao do Usuario 2", "Essa é a publicação do usuário 2! Oba!", 2),
+("Publicacao do Usuario 3", "Essa é a publicação do usuário 3! Oba!", 3);
