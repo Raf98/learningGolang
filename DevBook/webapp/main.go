955e060fd9f3fe52cf2650d66dbd594cdcb665a1
raf98 <rafaelrutzsantos@gmail.com>
2023-11-18 06:15:30 -0300

    Started coding webapp.

---
+++ DevBook/webapp/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"webapp/src/router"
+	"webapp/src/utils"
+)
+
+func main() {
+	utils.CarregarTemplates()
+	r := router.Gerar()
+
+	fmt.Println("Escutando na porta 3001")
+	log.Fatal(http.ListenAndServe(":3001", r))
+}
