a372787e45f0a3798e0caf116c22753bdbe0e8a3
raf98 <rafaelrutzsantos@gmail.com>
2024-01-03 04:07:35 -0300

    Finished lesson 132 (added bootstrap files to the project); added env vars, cookies and middleware to webapp.

--- DevBook/webapp/src/controllers/login.go
+++ DevBook/webapp/src/controllers/login.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/modelos"
 	"webapp/src/respostas"
 )
@@ -22,7 +25,8 @@ func FazerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, erro := http.Post("http://localhost:5001/login",
+	url := fmt.Sprintf("%s/login", config.APIURL)
+	response, erro := http.Post(url,
 		"/application/json", bytes.NewBuffer(usuario))
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
@@ -37,6 +41,11 @@ func FazerLogin(w http.ResponseWriter, r *http.Request) {
 
 	var dadosAutenticacao modelos.DadosAutenticacao
 	if erro = json.NewDecoder(response.Body).Decode(&dadosAutenticacao); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	if erro = cookies.Salvar(w, dadosAutenticacao.ID, dadosAutenticacao.Token); erro != nil {
 		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
 		return
 	}
