edcf18a92ea7be6c591bc336153a9cbcb6459b30
raf98 <rafaelrutzsantos@gmail.com>
2024-01-09 02:14:49 -0300

    Finished class 140; liking/ disliking implemented for the webapp.

--- DevBook/webapp/assets/js/publicacoes.js
+++ DevBook/webapp/assets/js/publicacoes.js
@@ -15,4 +15,61 @@ const criarPublicacao = (evento) => {
     });
 }
 
-$('#nova-publicacao').on('submit', criarPublicacao)
+const curtirPublicacao = (evento) => {
+    evento.preventDefault();
+
+    const elementoClicado = $(evento.target);
+    const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+    elementoClicado.prop('disabled', true);
+
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/curtir`,
+        method: 'POST'
+    }).done(() => {
+        const contadorDeCurtidas = elementoClicado.next('span');
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+        contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+
+        elementoClicado.addClass('descurtir-publicacao');
+        elementoClicado.addClass('text-danger');
+        elementoClicado.removeClass('curtir-publicacao');
+    }).fail(() => {
+        alert("Erro ao curtir a publicação!");
+    }).always(() => {
+        elementoClicado.prop('disabled', false);
+    });
+}
+
+const descurtirPublicacao = (evento) => {
+    evento.preventDefault();
+
+    const elementoClicado = $(evento.target);
+    const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+    elementoClicado.prop('disabled', true);
+
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/descurtir`,
+        method: 'POST'
+    }).done(() => {
+        const contadorDeCurtidas = elementoClicado.next('span');
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+        contadorDeCurtidas.text(quantidadeDeCurtidas - 1);
+
+        elementoClicado.removeClass('descurtir-publicacao');
+        elementoClicado.removeClass('text-danger');
+        elementoClicado.addClass('curtir-publicacao');
+    }).fail(() => {
+        alert("Erro ao descurtir a publicação!");
+    }).always(() => {
+        elementoClicado.prop('disabled', false);
+    });
+}
+
+$('#nova-publicacao').on('submit', criarPublicacao);
+
+$(document).on('click', '.curtir-publicacao', curtirPublicacao);
+$(document).on('click', '.descurtir-publicacao', descurtirPublicacao);
