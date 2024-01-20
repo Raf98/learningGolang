560c2eb16209bb8a03df8864ce0cc772e3ddb8c2
raf98 <rafaelrutzsantos@gmail.com>
2024-01-20 03:33:47 -0300

    Finished class 143; finished basic CRUD for publications.

--- DevBook/webapp/assets/js/publicacoes.js
+++ DevBook/webapp/assets/js/publicacoes.js
@@ -69,7 +69,54 @@ const descurtirPublicacao = (evento) => {
     });
 }
 
+function atualizarPublicacao() {
+    $(this).prop('disabled', true);
+
+    console.log($(this))
+    const publicacaoId = $(this).data('publicacao-id');
+    console.log(publicacaoId)
+
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}`,
+        method: "PUT",
+        data: {
+            titulo: $('#titulo').val(),
+            conteudo: $('#conteudo').val(),
+        }
+    }).done(() => {
+        alert("Publicação editada com sucesso!")
+    }).fail(() => {
+        alert("Erro ao editar a publicação!");
+    }).always(() => {
+        $('#atualizar-publicacao').prop('disabled', false);
+    })
+}
+
+function deletarPublicacao(evento) {
+    evento.preventDefault();
+
+    const elementoClicado = $(evento.target);
+    const publicacao =  elementoClicado.closest('div');
+    const publicacaoId = publicacao.data('publicacao-id');
+
+    elementoClicado.prop('disabled', true);
+
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}`,
+        method: 'DELETE'
+    }).done(() => {
+        publicacao.fadeOut("slow", function() {
+            $(this).remove();
+        });
+    }).fail(() => {
+        alert("Erro ao excluir a publicação!");
+    });
+}
+
 $('#nova-publicacao').on('submit', criarPublicacao);
 
 $(document).on('click', '.curtir-publicacao', curtirPublicacao);
 $(document).on('click', '.descurtir-publicacao', descurtirPublicacao);
+
+$('#atualizar-publicacao').on('click', atualizarPublicacao);
+$('.deletar-publicacao').on('click', deletarPublicacao);
